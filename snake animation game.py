2a1f3de890e079fc6c8ee0bc2ef222a39b84539f
Pacifique Nsabumukiza <133487528+Pacifique24-nsab@users.noreply.github.com>
2024-04-05 20:47:52 +0300

    Update snake animation game.py(comment)

--- snake animation game.py
+++ snake animation game.py
@@ -51,7 +51,8 @@ while True:
     next_key = w.getch()
     key = key if next_key == -1 else next_key
 
-    # Check if snake runs over the border or itself
+    # Check if snake runs over the border or itself( the area is limited, if the user bite the borders, the loop stop)
+    
     if snake[0][0] in [0, sh] or \
         snake[0][1]  in [0, sw] or \
         snake[0] in snake[1:]:
