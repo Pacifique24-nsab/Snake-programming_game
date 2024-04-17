8fc0eea238f45744c94f2bdd45a5891db4c162b0
Pacifique Nsabumukiza <133487528+Pacifique24-nsab@users.noreply.github.com>
2024-04-17 23:32:17 +0300

    Update snake animation game.py

--- snake animation game.py
+++ snake animation game.py
@@ -35,10 +35,10 @@ snk_y = sh//2
 snake = [
     [snk_y, snk_x],
     [snk_y, snk_x-1],
-    [snk_y, snk_x-2]
+    [snk_y, snk_x-2] 
 ]
 
-# Create the snake food
+# Create the snake food which
 food = [sh//2, sw//2]
 w.addch(int(food[0]), int(food[1]), curses.ACS_PI)
 
