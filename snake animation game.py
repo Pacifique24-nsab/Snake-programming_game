22476e68e45b6b742f7fda5f669a43e481c59f41
Pacifique Nsabumukiza <133487528+Pacifique24-nsab@users.noreply.github.com>
2024-04-20 21:21:03 +0300

    Update snake animation game.py

--- snake animation game.py
+++ snake animation game.py
@@ -6,7 +6,7 @@ import random
 print("Instructions:")
 print("Use the arrow keys to move the snake{ up-arrow,dow-arrow ,right-arrow, left-arrow .")
 print("The snake will move in the direction of the arrow key you press.")
-print("If the snake eats the food (π), it will grow longer.")
+print("If the snake eats the food which is (π), it will grow longer.")
 print("If the snake hits the border or itself, the game will end.")
 print("Enjoy the game!")
 # Initialize the screen
