3ffab0e6bbf3e944a14a419491e0f9acadc54629
Pacifique Nsabumukiza <133487528+Pacifique24-nsab@users.noreply.github.com>
2024-06-02 23:43:45 +0300

    Update snake animation game.py

--- snake animation game.py
+++ snake animation game.py
@@ -1,7 +1,7 @@
 import curses 
 import random
 # Print the instructions for the user  
-print("Instructions:")
+print("Instructions: ")
 print("Use the arrow keys to move the snake [up-arrow,dow-arrow ,right-arrow, left-arrow ."]
 print("The snake will move in the direction of the arrow key you press.")
 print("If the snake eats the food which is (π), it will grow longer.")
